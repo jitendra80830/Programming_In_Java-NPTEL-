dd45f489a3325cb041292dd000a1d58f1e4ed249
jitendra <jitendrakumar89973@gmail.com>
2021-02-20 18:51:54 +0530

    java Programming

---
+++ javaprogramming(2020)/Exersize15.java
@@ -0,0 +1,9 @@
+/*find the highest mark and average mark secured by Hari in "s" number of subjects.
+Private Test cases used for evaluation*/
+import java.util.Scanner;
+
+public class Exersize15{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		
+	
